295f3e7413160c66fafd95cd753b61fa34a674d2
develoHAM <kdw980722@gmail.com>
2023-11-13 03:44:50 +0900

    Feat: 헤더 추가

---
+++ client/src/components/Header.js
@@ -0,0 +1,23 @@
+import { Nav, Navbar, Container, Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+export default function Header() {
+	return (
+		<Navbar bg='light' data-bs-theme='light' fixed='top' className='py-3'>
+			<Container>
+				<Navbar.Brand className='text-success fw-bold'>
+					<img src='https://picsum.photos/40/40' className='d-inline-block rounded' alt='useMPD logo' />{' '}
+					useMDP
+				</Navbar.Brand>
+				<Nav>
+					<Button as={NavLink} to={'/roadmap'} className='mx-2' variant='success'>
+						My Roadmap
+					</Button>
+					<Button as={NavLink} to={'/login'} className='mx-2' variant='outline-success'>
+						Log In
+					</Button>
+				</Nav>
+			</Container>
+		</Navbar>
+	);
+}
