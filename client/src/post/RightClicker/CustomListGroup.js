808dbdaadf2c915390db94435cf7d0d5e96018c5
transfact <transpaction@naver.com>
2023-11-14 20:43:59 +0900

    Feat : DayPicker, RightClick

---
+++ client/src/post/RightClicker/CustomListGroup.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { useRef } from 'react';
+import ThumbnailMaker from './ThumbnailMaker';
+
+//props로 position을 줄것. 그럼 list그룹의 위치를 조절할 수 있다.
+const CustomListGroup = (props) => {
+    //실제 예제에서는 여러 방법으로 Ref를 가져와야함.
+    //혹은 html을 줘야함.
+
+    console.log(props);
+    const thumnnailRef = useRef(null);
+    function handleThumbnailDownload() {
+        console.log('download', thumnnailRef.current);
+        ThumbnailMaker(thumnnailRef);
+    }
+
+    return (
+        <ListGroup style={{ position: 'absolute', zIndex: 99, top: props.point[0], left: props.point[1] }} ref={thumnnailRef}>
+            <ListGroup.Item className="w-25 bg-dark" style={{ color: 'white' }}>
+                File
+            </ListGroup.Item>
+            <ListGroup.Item className="w-25 bg-dark" style={{ color: 'white' }}>
+                Link
+            </ListGroup.Item>
+            <ListGroup.Item onClick={handleThumbnailDownload} className="w-25 bg-dark" style={{ color: 'white' }}>
+                Save
+            </ListGroup.Item>
+            <ListGroup.Item className="w-25 bg-dark" style={{ color: 'white' }}>
+                Quit
+            </ListGroup.Item>
+        </ListGroup>
+    );
+};
+
+export default CustomListGroup;
---
+++ client/src/post/RightClicker/ThumbnailMaker.js
@@ -0,0 +1,21 @@
+import html2canvas from 'html2canvas';
+import saveAs from 'file-saver';
+import { useRef } from 'react';
+
+//사용법,ThumnbnailMaker에 Ref를 넣어주면, 그걸 그대로 png파일로 변환한다.
+// Ref란, <div ref = {customRef}> 에서 customRef는 useRef()로 만든 객체이다.
+export default async function ThumbnailMaker(targetRef) {
+    if (!targetRef.current) return;
+
+    try {
+        const div = targetRef.current;
+        const canvas = await html2canvas(div, { scale: 2 });
+        canvas.toBlob((blob) => {
+            if (blob !== null) {
+                saveAs(blob, 'results.png');
+            }
+        });
+    } catch (error) {
+        console.error('Error converting div to image:', error);
+    }
+}
