c3aa938cee4cfc418dce8683994d91f76d5fc2b8
develoHAM <kdw980722@gmail.com>
2023-11-17 14:55:58 +0900

    Refactor: 리액트 라우터

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
-import WelcomePage from '../views/WelcomePage';
+import WelcomePage from '../pages/WelcomePage';
+import HomePage from '../pages/HomePage';
+import MyPage from '../pages/MyPage';
+import PlannerPage from '../pages/PlannerPage';
 import App from '../App';
 
 const Router = createBrowserRouter([
@@ -10,6 +13,18 @@ const Router = createBrowserRouter([
 			{
 				index: true,
 				element: <WelcomePage />,
+			},
+			{
+				path: 'home',
+				element: <HomePage />,
+			},
+			{
+				path: 'mypage',
+				element: <MyPage />,
+			},
+			{
+				path: 'planner',
+				element: <PlannerPage />,
 			},
 		],
 	},
