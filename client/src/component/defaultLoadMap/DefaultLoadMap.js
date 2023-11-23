620fc77b41cec98306e876f332b877179d6483f5
transfact <transpaction@naver.com>
2023-11-23 19:14:40 +0900

    중간저장

--- client/src/component/defaultLoadMap/DefaultLoadMap.js
+++ client/src/component/defaultLoadMap/DefaultLoadMap.js
@@ -1,12 +1,43 @@
-// import DefaultLoadMap from '../component/home/DefaultLoadMap';
 import { useState } from 'react';
 import DefaultComponent from './DefaultComponent';
 import StarComponent from '../starComponent/StarComponent';
 import HomeComponent from '../homeComponent/HomeComponent';
 import MyCalendar from '../home/calendar/MyCalendar';
 import SearchComponent from '../searchComponent/SearchComponent';
+import styled from 'styled-components';
+const _Button = styled.button`
+    border: none;
+    background: none;
+    /* color: gray; */
+
+    /* &:active {
+    color: black;
+  }
+  &:focus {
+    color: black;
+  } */
+`;
+
+const _Flex = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+const _FlexNavi = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    width: 100%;
+    align-items: flex-start;
+`;
+
+const _RenderComponent = styled.div`
+    z-index: 50;
+`;
+
 export default function DefaultLoadMap() {
     const [menuNumber, setMenuNumber] = useState();
+    const [isActive, setIsActive] = useState(false);
     const renderComponent = () => {
         switch (menuNumber) {
             case 1:
@@ -16,9 +47,9 @@ export default function DefaultLoadMap() {
             case 3:
                 return <StarComponent />;
             case 4:
-                return <MyCalendar />;
-            case 5:
                 return <SearchComponent />;
+            case 5:
+                return <MyCalendar />;
             default:
                 return <HomeComponent />;
         }
@@ -27,89 +58,58 @@ export default function DefaultLoadMap() {
         setMenuNumber(number);
     };
 
-const _Button = styled.button`
-  border: none;
-  background: none;
-  /* color: gray; */
-
-  /* &:active {
-    color: black;
-  }
-  &:focus {
-    color: black;
-  } */
-`;
-
-const _Flex = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const _FlexNavi = styled.div`
-  display: flex;
-  flex-direction: column;
-  position: fixed;
-  width: 100%;
-  align-items: flex-start;
-`;
-
-const _RenderComponent = styled.div`
-  z-index: 50;
-`;
-
-export default function DefaultLoadMap() {
-  const [menuNumber, setMenuNumber] = useState();
-  const [isActive, setIsActive] = useState(false);
-  const renderComponent = () => {
-    switch (menuNumber) {
-      case 1:
-        return <HomeComponent />;
-      case 2:
-        return <DefaultComponent />;
-      case 3:
-        return <StarComponent />;
-      case 4:
-        return <SearchComponent />;
-      case 5:
-        return <CalendarCompo />;
-      default:
-        return <HomeComponent />;
-    }
-  };
-  const handleNumber = (number) => {
-    setMenuNumber(number);
-  };
-
-  return (
-    <_Flex>
-      <_FlexNavi>
-        <_Button className="default" onClick={() => handleNumber(1)}>
-          <i class="material-icons" style={{ fontSize: "40px" }}>
-            description
-          </i>
-        </_Button>
-        <_Button className="default" onClick={() => handleNumber(2)}>
-          <i class="material-icons" style={{ fontSize: "40px" }}>
-            star
-          </i>
-        </_Button>
-        <_Button className="default" onClick={() => handleNumber(3)}>
-          <i class="material-icons" style={{ fontSize: "40px" }}>
-            account_circle
-          </i>
-        </_Button>
-        <_Button className="default" onClick={() => handleNumber(4)}>
-          <i class="material-icons" style={{ fontSize: "40px" }}>
-            calendar_month
-          </i>
-        </_Button>
-        <_Button className="default" onClick={() => handleNumber(5)}>
-          <i class="material-icons" style={{ fontSize: "40px" }}>
-            search
-          </i>
-        </_Button>
-      </_FlexNavi>
-      <_RenderComponent>{renderComponent()}</_RenderComponent>
-    </_Flex>
-  );
+    return (
+        <_Flex>
+            <_FlexNavi>
+                <_Button className="default" onClick={() => handleNumber(1)}>
+                    <i class="material-icons" style={{ fontSize: '40px' }}>
+                        description
+                    </i>
+                </_Button>
+                <_Button className="default" onClick={() => handleNumber(2)}>
+                    <i class="material-icons" style={{ fontSize: '40px' }}>
+                        star
+                    </i>
+                </_Button>
+                <_Button className="default" onClick={() => handleNumber(3)}>
+                    <i class="material-icons" style={{ fontSize: '40px' }}>
+                        account_circle
+                    </i>
+                </_Button>
+                <_Button className="default" onClick={() => handleNumber(4)}>
+                    <i class="material-icons" style={{ fontSize: '40px' }}>
+                        calendar_month
+                    </i>
+                </_Button>
+                <_Button className="default" onClick={() => handleNumber(5)}>
+                    <i class="material-icons" style={{ fontSize: '40px' }}>
+                        search
+                    </i>
+                </_Button>
+            </_FlexNavi>
+            <_RenderComponent>{renderComponent()}</_RenderComponent>
+        </_Flex>
+    );
 }
+
+// export default function DefaultLoadMap() {
+//   const [menuNumber, setMenuNumber] = useState();
+//   const renderComponent = () => {
+//       switch (menuNumber) {
+//           case 1:
+//               return <HomeComponent />;
+//           case 2:
+//               return <DefaultComponent />;
+//           case 3:
+//               return <StarComponent />;
+//           case 4:
+//               return <MyCalendar />;
+//           case 5:
+//               return <SearchComponent />;
+//           default:
+//               return <HomeComponent />;
+//       }
+//   };
+//   const handleNumber = (number) => {
+//       setMenuNumber(number);
+//   };
