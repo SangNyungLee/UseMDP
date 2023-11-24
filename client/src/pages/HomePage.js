629338dbaf6b57e00783c539f0a8d81968ecbab5
transfact <transpaction@naver.com>
2023-11-24 16:44:53 +0900

    api tests

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,25 +1,25 @@
-import { Outlet } from "react-router";
-import HomeSideBar from "../component/home/HomeSideBar";
-import "../constant/css/index.css";
-import styled from "styled-components";
+import { Outlet } from 'react-router';
+import HomeSideBar from '../component/home/HomeSideBar';
+import '../constant/css/index.css';
+import styled from 'styled-components';
 
 const _Font = styled.div`
-  font-family: "SUITE-Regular";
+    font-family: 'SUITE-Regular';
 `;
 
 const _Flex = styled.div`
-  display: flex;
-  justify-content: center;
+    display: flex;
+    justify-content: center;
 `;
 
 export default function HomePage() {
-  return (
-    <_Font>
-      <div>HomePage</div>
-      <_Flex>
-        <HomeSideBar />
-        <Outlet />
-      </_Flex>
-    </_Font>
-  );
+    return (
+        <_Font>
+            <div>HomePage</div>
+            <_Flex>
+                {/* <HomeSideBar /> */}
+                <Outlet />
+            </_Flex>
+        </_Font>
+    );
 }
