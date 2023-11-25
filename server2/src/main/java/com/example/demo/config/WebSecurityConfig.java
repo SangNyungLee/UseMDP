f557f51375e187954731d4d909c0e4ba765a52b5
transfact <transpaction@naver.com>
2023-11-25 12:08:55 +0900

    local

--- server2/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ server2/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig {
 
         config.setAllowCredentials(true);
         config.setAllowedOrigins(Arrays.asList("https://www.usemdp.site", "http://localhost:3000"));
-        config.setAllowedMethods(Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT"));
+        config.setAllowedMethods(Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT","PATCH"));
         config.setAllowedHeaders(Arrays.asList("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
