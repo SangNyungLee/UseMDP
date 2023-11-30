b8c4b5e45f4f73251d0f06c632547313e5dadcbd
develoHAM <kdw980722@gmail.com>
2023-11-30 19:39:03 +0900

    Design: CustomList 작업 시작

---
+++ client/src/constant/css/styledComponents/__DefaultLoadMap.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { Row, Col, Stack, Container, Nav, Navbar, Button } from 'react-bootstrap';
+
+export const _Container = styled(Container)``;
+
+export const _Row = styled(Row)``;
+
+export const _SidebarCol = styled(Col)``;
+
+export const _RenderComponentCol = styled(Col)``;
+
+export const _RenderComponent = styled.div`
+	z-index: 50;
+`;
+
+export const _Navbar = styled(Navbar)``;
+
+export const _Nav = styled(Nav)``;
+
+export const _NavItem = styled(Nav.Item)``;
+
+export const _Button = styled(Button)``;
