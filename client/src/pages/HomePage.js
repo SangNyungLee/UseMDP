ae49ca4d58483350e9a15087f2928c8b897a37ec
김성제 <aymrm@naver.com>
2023-12-04 21:09:56 +0900

    최적화

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,15 +1,9 @@
 import { Outlet } from 'react-router';
 import '../constant/css/index.css';
 import styled from 'styled-components';
-import CalendarSideBar from '../component/home/calendar/CalendarSideBar';
-import Header from '../component/Header';
 import { useDispatch } from 'react-redux';
 import { pointActions } from '../store/pointer';
-import useDefaultCheck from '../hook/useDefaultCheck';
-import { HOME } from '../constant/constant';
-import { useSelector } from 'react-redux';
-import HomeHeader from '../component/home/HomeHeader';
-import ThemeProvider from 'react-bootstrap/ThemeProvider';
+
 import { _PageContainer } from '../constant/css/styledComponents/__HomePage';
 import RealHeader from '../component/RealHeader';
 
@@ -27,12 +21,9 @@ export default function HomePage() {
 	};
 
 	return (
-		<ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']} minBreakpoint='xxs'>
-			<_PageContainer id='HOMEPAGE' onClick={(e) => handlePoint(e)}>
-				{/* <HomeHeader /> */}
-				<RealHeader />
-				<Outlet />
-			</_PageContainer>
-		</ThemeProvider>
+		<_PageContainer id='HOMEPAGE' onClick={(e) => handlePoint(e)}>
+			<RealHeader />
+			<Outlet />
+		</_PageContainer>
 	);
 }
